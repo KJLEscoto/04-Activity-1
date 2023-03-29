55cfd84729c030fea4d9f74ae248f76e158ea4dc
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2023-03-29 19:57:53 +0800

    my-update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        firaSans: ['Fira Sans Extra Condensed'],
+        germaniaOne: ['Germania One']
+      }
+    },
+  },
+  plugins: [],
+}
